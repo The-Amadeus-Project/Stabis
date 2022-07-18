226f4035b94394255ac5e39fd9ce3a200134bbfd
CunningBard <jhonjhonjumaday@gmail.com>
2022-07-19 01:21:43 +0800

    run the formatter for the file

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
+use crate::sb_is::{compile, interpret};
 use std::env::args;
 use std::io;
 use std::io::Write;
-use std::process::{Command, exit};
-use crate::sb_is::{compile, interpret};
+use std::process::{exit, Command};
+mod base;
+mod compiler;
+mod interpreter;
 mod lexer;
 mod sb_is;
-mod interpreter;
-mod compiler;
-mod base;
 mod type_checker;
 // mod check;
 
-fn usage(){
+fn usage() {
     let message = r#"------------USAGE------------
 command filepath [mode] [etc]
 
@@ -37,34 +37,43 @@ fn main() {
     let mode = args.remove(0);
     let mut debug = false;
     for arg in args {
-        if arg == "--debug"{
+        if arg == "--debug" {
             debug = true;
         }
     }
     if mode == "-c" {
         let result = compile(file_path.clone(), debug);
         let mut file_res = std::fs::File::create(file_path.replace(".sbis", ".rs"));
-        if file_res.is_err(){
+        if file_res.is_err() {
             std::fs::write(file_path.replace(".sbis", ".rs"), result).unwrap();
         } else {
             let mut file = file_res.unwrap();
             let res = file.write_all(result.as_ref());
         }
         Command::new("rustc")
-            .args([&*file_path.replace(".sbis", ".rs"), "-o", &*file_path.replace(".sbis", "")])
+            .args([
+                &*file_path.replace(".sbis", ".rs"),
+                "-o",
+                &*file_path.replace(".sbis", ""),
+            ])
             .output()
             .expect("failed to execute process");
     } else if mode == "-r" {
         let result = compile(file_path.clone(), debug);
         let mut file_res = std::fs::File::create(file_path.replace(".sbis", ".rs"));
-        if file_res.is_err(){
+        if file_res.is_err() {
             std::fs::write(file_path.replace(".sbis", ".rs"), result).unwrap();
         } else {
             let mut file = file_res.unwrap();
             let res = file.write_all(result.as_ref());
         }
         Command::new("rustc")
-            .args([&*file_path.replace(".sbis", ".rs"), "-o", &*file_path.replace(".sbis", "")])
+            .args([
+                &*file_path.replace(".sbis", ".rs"),
+                "-o",
+                &*file_path.replace(".sbis", ""),
+                "-q",
+            ])
             .output()
             .expect("failed to execute process");
     } else if mode == "-i" {
