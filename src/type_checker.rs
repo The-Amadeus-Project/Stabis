c77f65b068067d6c58d43f7cabbc580692bfdcab
CunningBard <jhonjhonjumaday@gmail.com>
2022-07-19 01:22:24 +0800

    run the formatter on the file

--- src/type_checker.rs
+++ src/type_checker.rs
@@ -1,15 +1,15 @@
-use std::collections::HashMap;
 use crate::lexer::Token;
+use std::collections::HashMap;
 
 enum ValTypes {
     Int,
     Float,
     String,
-    Boolean
+    Boolean,
 }
 
 struct Checker {
     instructions: Vec<Token>,
     defined_functions: HashMap<String, i32>,
-    index: i64
+    index: i64,
 }
