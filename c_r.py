f2187c7d68c006d356c2f5054636174565ca0e3b
CunningBard <jhonjhonjumaday@gmail.com>
2022-07-19 01:12:29 +0800

    added more error handling

--- c_r.py
+++ c_r.py
@@ -4,7 +4,16 @@ import sys
 """
 python c_r.py filepath.sbis
 """
+
+if sys.argv == 1:
+    print("Expected File Path")
+elif sys.argv > 2:
+    print("Expected only File Path as argument")
 file = sys.argv[1]
 
-subprocess.run(["cargo", "run", "--", file, "-c"])
-subprocess.run(["./" + file.replace(".sbis", "")])
+
+return_code = subprocess.run(["cargo", "run", "--", file, "-c"])
+if return_code.returncode:
+    print(f"ERROR OCCURRED {return_code.stderr}")
+else:
+    subprocess.run(["./" + file.replace(".sbis", "")])
